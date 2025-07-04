0562be7ed3fed751093611f63c78120b03877b4f
mwordlling <beachnikko44@gmail.com>
2025-07-04 10:47:20 +0300

    Updates

--- _app/immutable/chunks/paths.3c117dd1.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_150f0v3)==null?void 0:s.base)??"/mentimeter";var e;const a=((e=globalThis.__sveltekit_150f0v3)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/chunks/paths.869e0a90.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1osr5bt)==null?void 0:s.base)??"/mentimeter";var t;const a=((t=globalThis.__sveltekit_1osr5bt)==null?void 0:t.assets)??e;export{a,e as b};
